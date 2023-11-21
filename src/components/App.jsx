4b63ded46f0d80d0d168c2ab1606bbbdd795bab0
DipaliDuratkar1999 <dipaliduratkar1999@gmail.com>
2023-11-21 19:07:05 +0530

    test

---
+++ src/components/App.jsx
@@ -0,0 +1,37 @@
+import useUpdatingClock from 'hooks/useUpdatingClock'
+
+export default function App() {
+  const {hour, minutes, seconds, amPm} = useUpdatingClock()
+
+  return (
+    <div className="df flex-col vh-100">
+     
+
+      <section className="flex-grow-1 bg-black-80 fw4 white-80 tc pt24">
+        <div>
+          Your application starts in the{' '}
+          <code>
+            src/<span className="b white">entry.jsx</span>
+          </code>{' '}
+          file.
+        </div>
+
+        <div>
+          The component you're looking here at can be found in{' '}
+          <code>
+            src/components/<span className="b white">App.jsx</span>
+          </code>
+        </div>
+
+        <div>
+          Now go! Save the world with <span className="gold">JavaScript</span>!
+        </div>
+
+        <div className="pa16 f-1-5em">
+          {hour}:{minutes}:{seconds}
+          <span className="f-initial pl4">{amPm}</span>
+        </div>
+      </section>
+    </div>
+  )
+}
---
+++ src/components/Dipali.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Dipali=()=>{
+    return(
+        <div>
+            <h1>dipali</h1>
+        </div>
+    )
+}
+export default Dipali;
